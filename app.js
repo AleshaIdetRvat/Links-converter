1aa14941fe23e2d96447044f8398e724dba7ede9
Alexey <alex967268@gmail.com>
2021-11-08 22:13:10 +0300

    fix deploy issue

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
 const express = require("express")
 const config = require("config")
 const mongoose = require("mongoose")
-const path = require("path")
 const app = express()
 
-app.use((req, res, next) => {
-    res.append("Access-Control-Allow-Origin", ["*"])
-    res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-    res.append(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    )
-    next()
-}) // CORS
+const cors = require("cors")
+require("dotenv").config()
+
+const PORT = process.env.PORT || 5000
+
+app.use(cors()) // CORS
 
 app.use(express.json({ extended: true }))
 
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/link", require("./routes/link.routes"))
 app.use("/t", require("./routes/redirect.routes"))
-
-if (process.env.NODE_ENV === "production") {
-    app.use("/", express.static(path.join(__dirname, "client", "build")))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    })
-}
-
-const PORT = 5000 //config.get("port") || 5001
 
 async function start() {
     try {
@@ -42,4 +28,5 @@ async function start() {
         process.exit
     }
 }
+
 start()
